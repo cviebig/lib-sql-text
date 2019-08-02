9a0b839c47f2c1a3044b94dca4cb1e0ee3ad9dbe
Christoph Viebig <git@christoph-viebig.de>
2019-08-02 19:51:42 +0200

    Fix breaking change in Boost 1.67 regarding `boost::spirit::x3::context`

--- include/sql/text/parser/config.hpp
+++ include/sql/text/parser/config.hpp
@@ -22,6 +22,22 @@ namespace sql { namespace text {
     using error_handler_type = boost::spirit::x3::error_handler<iterator_type>;
 
     // Combined Error Handler and Phrase Parse Context
+    //
+    // Definition differs between Boost version < 1.67 and >= 1.67 due to
+    // breaking change:
+    // https://www.boost.org/users/history/version_1_67_0.html
+    // https://github.com/boostorg/spirit/pull/239
+    // https://github.com/boostorg/spirit/pull/254
+#if BOOST_VERSION >= 106700
+    using context_type =
+        boost::spirit::x3::context<
+            boost::spirit::x3::error_handler_tag,
+            std::reference_wrapper<error_handler_type> const,
+            boost::spirit::x3::phrase_parse_context<
+                boost::spirit::x3::ascii::space_type
+            >::type
+        >;
+#else
     using context_type =
         boost::spirit::x3::with_context<
             boost::spirit::x3::error_handler_tag,
@@ -30,7 +46,7 @@ namespace sql { namespace text {
                 boost::spirit::x3::ascii::space_type
             >::type
         >::type;
-
+#endif
 }}
 
-#endif
+#endif //SQL_TEXT_PARSER_CONFIG_HPP
